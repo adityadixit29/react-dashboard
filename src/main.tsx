65251a5e1e38d74015fbc1a498c10a79cc60a18d
Aditya Dixit <dixitaditya2001@gmail.com>
2023-12-12 04:56:17 +0530

    intial-commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+export const server = "https://react-dashboard-backend-l8io.onrender.com/api/v1/sector";
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
